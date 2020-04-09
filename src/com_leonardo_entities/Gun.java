ffffbcc2c6a39e717241a46471b320b5a52b1a0c
leoreisdias <leoanrdord99@gmail.com>
2020-04-09 12:42:34 -0300

    Gun Animation
    Uma simples e pequena animação adicionada a arma
    para melhores efeitos visuais
    
    Seguiu os mesmos conceitos da animação do inimigo.

--- src/com_leonardo_entities/Gun.java
+++ src/com_leonardo_entities/Gun.java
@@ -1,12 +1,36 @@
 package com_leonardo_entities;
 
 import java.awt.image.BufferedImage;
+import java.awt.Graphics;
+import com_leonardo_main.Game;
+import com_leonardo_world.Camera;
 
 public class Gun extends Entity {
 
+    private BufferedImage[] gunSprite;
+    private int frames = 0, maxFrames = 15, index = 0, maxIndex = 1;
+
     public Gun(int x, int y, int width, int height, BufferedImage sprite) {
         super(x, y, width, height, sprite);
-        // TODO Auto-generated constructor stub
+        gunSprite = new BufferedImage[2];
+        gunSprite[0] = Game.spritesheet.getSprite(112, 0, 16, 16);
+        gunSprite[1] = Game.spritesheet.getSprite(112 + 16, 0, 16, 16);
+    }
+
+    public void tick() {
+        frames++;
+        if (frames == maxFrames) {
+            frames = 0;
+            index++;
+            if (index > maxIndex) {
+                index = 0;
+            }
+        }
+
+    }
+
+    public void render(Graphics g) {
+        g.drawImage(gunSprite[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
     }
 
 }
